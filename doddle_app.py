1d5b8832723332036d2b8a0d3a8863446869e8e1
Unknown <matthewdanieldenvir@gmail.com>
2018-06-27 00:12:55 +0100

    Flask framework
    Initial Flask Framework with changes

---
+++ doddle_app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8082, host='127.0.0.1')
